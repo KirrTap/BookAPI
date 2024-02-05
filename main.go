dc8e7d0de0ac11f11500fb156985386fd4fffb77
Patrik Cmil <patrik.cmil@student.tuke.sk>
2024-02-05 18:11:27 +0100

    pokus c.2

--- main.go
+++ main.go
@@ -1,87 +1,33 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"gopkg.in/yaml.v2"
-	"io/ioutil"
-	"net/http"
-	"strings"
-	"sort"
+	"flag"
 )
 
-const BookAPI = "https://openlibrary.org/search.json?q=%s"
-
-type AuthorBook struct {
-	Docs []struct {
-		AuthorName 					     []string `json:"author_name,omitempty"`
-		Title                            string   `json:"title"`
-		PublishDate                      []string `json:"publish_date,omitempty"`
-		EditionCount                     int      `json:"edition_count"`
-		Isbn                             []string `json:"isbn,omitempty"`
-		
-	} `json:"docs"`
-}
+const BookAPI = "https://openlibrary.org/works/%s.json"
 
 
 func main() {
-	bookTitle := "1984"
-	sortOrder := "desc"
+	bookKey := flag.String("key", "", "Book key")     //OL27448W 
+	sortOrder := flag.String("sort", "", "Sort order (asc or desc)")
 	
-	resp, err := http.Get(fmt.Sprintf(BookAPI, strings.Replace(bookTitle, " ", "+", -1)))
 
-	if err != nil {
-		fmt.Println("No response from request")
-	}
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-
-	var result AuthorBook
-	if err := json.Unmarshal(body, &result); err != nil {
-		fmt.Println("Can not unmarshal JSON")
-	}
-
-
-	AuthorNameSort(sortOrder, result)
-	PublishDateSort(sortOrder, result)
-
-	yamlData, err := yaml.Marshal(result)
-	if err != nil {
-		fmt.Println("Error converting to YAML:", err)
+	flag.Parse()
+	if *bookKey == "" {
+		fmt.Println("Error: Specify book key with -key.")
 		return
 	}
 
-	fmt.Println(string(yamlData))
-
-}
-
-
-func AuthorNameSort(sortOrder string, result AuthorBook) {
-	for _, sorted := range result.Docs {
-		if sortOrder == "asc" {
-			if len(sorted.AuthorName) > 0 {
-				sort.Strings(sorted.AuthorName)
-			}
-		} else if sortOrder == "desc" {
-			if len(sorted.AuthorName) > 0 {
-				sort.Sort(sort.Reverse(sort.StringSlice(sorted.AuthorName)))
-			}
-		}
+	if *sortOrder != "" && *sortOrder != "asc" && *sortOrder != "desc" {
+		fmt.Println("Error: You need to specify a valid sort order -sort (asc or desc).")
+		return
 	}
-	
+
+	bookAPI := fmt.Sprintf(BookAPI, *bookKey)
+
+	fmt.Println(*bookKey)
+	fmt.Println(*sortOrder)
+	fmt.Println(bookAPI)
 }
 
-func PublishDateSort(sortOrder string, result AuthorBook) {
-	for _, sorted := range result.Docs {
-		if sortOrder == "asc" {
-			if len(sorted.PublishDate) > 0 {
-				sort.Strings(sorted.PublishDate)
-			}
-		} else if sortOrder == "desc" {
-			if len(sorted.PublishDate) > 0 {
-				sort.Sort(sort.Reverse(sort.StringSlice(sorted.PublishDate)))
-			}
-		}
-	}
-}
